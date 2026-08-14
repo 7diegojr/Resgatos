96d34d99a350e0e07e2391cb768b3f324f5b4734
7diegojr <dfsntsjunior@gmail.com>
2026-08-14 12:16:06 -0300

    Configura conexão com MySQL e inicialização do banco

--- Resgatos/Program.cs
+++ Resgatos/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Resgatos.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do Entity Framework Core com MySQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySQL(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -14,16 +24,21 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// Criação automática do banco de dados e das tabelas
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await context.Database.EnsureCreatedAsync();
+}
 
 app.Run();
